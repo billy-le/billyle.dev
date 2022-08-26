6c6eacf281e240a62cb4fcbf227f56dbeb254f5b
billy le <lebilly87@gmail.com>
2022-08-26 11:10:10 -0700

    add sitemap plugin

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from "@astrojs/image";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  site: 'https://billyle.dev',
+  integrations: [tailwind(), image(), sitemap()]
 });
