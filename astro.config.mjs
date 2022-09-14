dc72c7fc2f86c2beaaf97310588ff18070a0e6a2
billy le <lebilly87@gmail.com>
2022-09-14 09:54:29 -0700

    blog: prepared site for blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://billyle.dev',
-  integrations: [tailwind(), image(), sitemap()]
+  site: "https://billyle.dev",
+  integrations: [tailwind(), image(), sitemap(), mdx()],
 });
