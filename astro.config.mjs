0d8ce54f6a78c3d77f5a777e90cb854e2df9f2b8
billy le <lebilly87@gmail.com>
2023-01-27 23:02:18 -0800

    upgraded astro and fixed images

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://billyle.dev',
   integrations: [tailwind(), image(), sitemap()]
 });
