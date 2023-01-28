e55dcf172eab3204f45aadecab1a883510b1f87c
billy le <lebilly87@gmail.com>
2023-01-27 23:55:06 -0800

    updated resume and work experience section. add site config back in

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://billyle.dev',
   integrations: [tailwind(), image(), sitemap()]
 });
