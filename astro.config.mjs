6194a8516bfb01f3e4571d71504ae405ec872e83
Billy Le <hi@billyle.dev>
2023-12-12 17:07:52 +0800

    migrate to astro/v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import image from "@astrojs/image";
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://billyle.dev',
-  integrations: [tailwind(), image(), sitemap()]
+  site: "https://billyle.dev",
+  integrations: [tailwind(), sitemap()],
 });
