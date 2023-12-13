29600e5d819c2731fc26af7ac33f8f8c7e12c567
Billy Le <hi@billyle.dev>
2023-12-13 10:46:11 +0800

    added skills section and prettified codebase

--- packages/website/.prettierrc.js
+++
@@ -1,3 +0,0 @@
-{
-    astroAllowShorthand: true;
-  }
--- packages/website/tailwind.config.mjs
+++ packages/website/tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
-import typography from '@tailwindcss/typography'
+import typography from "@tailwindcss/typography";
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		container: {
-			padding: {
-				DEFAULT: '1rem',
-			},
-			center: true
-		},
-	},
-	plugins: [typography()],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+      },
+      center: true,
+    },
+  },
+  plugins: [typography()],
+};
