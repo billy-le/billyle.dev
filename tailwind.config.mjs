806fcd2d7ba55b05fab6c531cabb706c302b4b70
Billy Le <hi@billyle.dev>
2024-02-14 12:01:23 +0800

    add theme toggle and stylized dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-
 import typography from "@tailwindcss/typography";
 
+/** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     container: {
