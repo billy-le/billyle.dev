a8ad8d0f217a92c71289a8baf01dcb6c098d6b42
Billy Le <hi@billyle.dev>
2024-03-03 16:01:30 +0800

    completed self-hosting blog

--- packages/website/src/content/config.ts
+++ packages/website/src/content/config.ts
@@ -1,20 +1,43 @@
 import { z, defineCollection } from "astro:content";
 
+const validTags = [
+  "blogging",
+  "content creation",
+  "devops",
+  "golang",
+  "javascript",
+  "learn-along",
+  "paas",
+  "self-hosting",
+  "tutorial",
+];
+
+const postSchema = z.object({
+  title: z.string(),
+  pubDate: z.date(),
+  description: z.string().min(200),
+  author: z.string().default("Billy Le"),
+  image: z.object({
+    url: z.string(),
+    alt: z.string(),
+    className: z.string().optional(),
+  }),
+  tags: z
+    .array(
+      z.string().refine(
+        (tag) => validTags.includes(tag),
+        (tag) => ({ message: `'${tag}' is not a valid tag` }),
+      ),
+    )
+    .nonempty(),
+  draft: z.boolean(),
+});
+
+export type Post = z.infer<typeof postSchema>;
+
 const postsCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string().min(200),
-    author: z.string().default("Billy Le"),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-      className: z.string().optional(),
-    }),
-    tags: z.array(z.string()).nonempty(),
-    draft: z.boolean(),
-  }),
+  schema: postSchema,
 });
 
 export const collections = {
