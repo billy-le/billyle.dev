8c31650a58c5f67c90f1d41cdd6bef03901ac9ba
Billy Le <hi@billyle.dev>
2024-03-04 15:06:57 +0800

    add robot txt integration

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 /** @type {import('astro/config').AstroUserConfig} */
 export default defineConfig({
   site: "https://billyle.dev",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   prefetch: true,
 });
