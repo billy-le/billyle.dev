1c18475f3e9467e95b7cda6b4fbe78afc4ca51d5
Billy Le <hi@billyle.dev>
2024-03-05 09:56:09 +0800

    fix footer blog link, About text body color, and BlogCard line-clamp

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,13 @@ import robotsTxt from "astro-robots-txt";
 /** @type {import('astro/config').AstroUserConfig} */
 export default defineConfig({
   site: "https://billyle.dev",
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [
+    tailwind({
+      nesting: true,
+    }),
+    sitemap(),
+    robotsTxt(),
+  ],
   prefetch: true,
   redirects: {
     "/posts": "/blog",
