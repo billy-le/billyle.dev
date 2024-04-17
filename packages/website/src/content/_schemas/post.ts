82d68538706f30ebbdd2fa0114c1e1519b20e9af
Billy Le <hi@billyle.dev>
2024-04-17 11:41:12 +0800

    add project collection

---
+++ packages/website/src/content/_schemas/post.ts
@@ -0,0 +1,54 @@
+import { z, defineCollection } from "astro:content";
+
+const validTags = [
+  "blogging",
+  "content creation",
+  "devops",
+  "golang",
+  "javascript",
+  "learn-along",
+  "paas",
+  "self-hosting",
+  "tutorial",
+  "mobile development",
+  "iOS",
+  "node",
+  "bun",
+  "astro",
+];
+
+const postSchema = z.object({
+  title: z.string(),
+  pubDate: z.date(),
+  description: z.string().min(200),
+  author: z
+    .object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+    .default({
+      name: "Billy Le",
+      email: "hi@billyle.dev",
+    }),
+  image: z.object({
+    url: z.string(),
+    alt: z.string(),
+    className: z.string().optional(),
+  }),
+  tags: z
+    .array(
+      z.string().refine(
+        (tag) => validTags.includes(tag),
+        (tag) => ({ message: `'${tag}' is not a valid tag` }),
+      ),
+    )
+    .nonempty(),
+  draft: z.boolean(),
+});
+
+export const postsCollection = defineCollection({
+  type: "content",
+  schema: postSchema,
+});
+
+export type Post = z.infer<typeof postSchema>;
--- packages/website/src/content/config.ts
+++ packages/website/src/content/config.ts
@@ -1,58 +1,7 @@
-import { z, defineCollection } from "astro:content";
-
-const validTags = [
-  "blogging",
-  "content creation",
-  "devops",
-  "golang",
-  "javascript",
-  "learn-along",
-  "paas",
-  "self-hosting",
-  "tutorial",
-  "mobile development",
-  "iOS",
-  "node",
-  "bun",
-  "astro",
-];
-
-const postSchema = z.object({
-  title: z.string(),
-  pubDate: z.date(),
-  description: z.string().min(200),
-  author: z
-    .object({
-      name: z.string(),
-      email: z.string().email(),
-    })
-    .default({
-      name: "Billy Le",
-      email: "hi@billyle.dev",
-    }),
-  image: z.object({
-    url: z.string(),
-    alt: z.string(),
-    className: z.string().optional(),
-  }),
-  tags: z
-    .array(
-      z.string().refine(
-        (tag) => validTags.includes(tag),
-        (tag) => ({ message: `'${tag}' is not a valid tag` }),
-      ),
-    )
-    .nonempty(),
-  draft: z.boolean(),
-});
-
-export type Post = z.infer<typeof postSchema>;
-
-const postsCollection = defineCollection({
-  type: "content",
-  schema: postSchema,
-});
+import { postsCollection } from "./_schemas/post";
+import { projectCollections } from "./_schemas/project";
 
 export const collections = {
   posts: postsCollection,
+  projects: projectCollections,
 };
