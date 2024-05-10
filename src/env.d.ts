c264e4372a89fae3362049ded7138a10ef68d28b
Billy Le <hi@billyle.dev>
2024-05-10 18:27:29 +0800

    remark42 wip

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_REMARK42_URL: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
