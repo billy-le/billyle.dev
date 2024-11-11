2da6e1e182df7aed581be6db19093b7e990084f4
Billy Le <hi@billyle.dev>
2024-11-11 16:25:50 +0800

    use last .git modified date

--- remark-plugins/remark-last-date-modified.ts
+++ remark-plugins/remark-last-date-modified.ts
@@ -1,11 +1,11 @@
-import fs from "node:fs/promises";
-
+import { execSync } from "node:child_process";
 import type { RemarkPlugins } from "astro";
 
 export const remarkLastDateModified: RemarkPlugins[number] = function () {
-  return async function (_, file) {
-    const data = await fs.stat(file.path);
-    // @ts-ignore
-    file.data.astro.frontmatter.lastDateModified = data.mtime;
-  };
+	return async function (_, file) {
+		const filepath = file.history[0];
+		const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+		// @ts-ignore
+		file.data.astro.frontmatter.lastDateModified = result.toString();
+	};
 };
