13c567bfca9e15d5c4cbbaf856b1e9946bc9a766
Billy Le <hi@billyle.dev>
2024-11-19 16:38:17 +0800

    format files

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,21 +1,21 @@
 export function debounce<Args extends any[], R = void>(
-	func: (args: Args) => R,
-	delay: number,
+  func: (args: Args) => R,
+  delay: number,
 ): [(args: Args) => Promise<R>, () => void] {
-	let timer: ReturnType<typeof setTimeout> | undefined;
+  let timer: ReturnType<typeof setTimeout> | undefined;
 
-	const debouncedFunc = (args: Args): Promise<R> =>
-		new Promise((resolve) => {
-			if (timer) {
-				clearTimeout(timer);
-			}
+  const debouncedFunc = (args: Args): Promise<R> =>
+    new Promise((resolve) => {
+      if (timer) {
+        clearTimeout(timer);
+      }
 
-			timer = setTimeout(() => {
-				resolve(func(args));
-			}, delay);
-		});
+      timer = setTimeout(() => {
+        resolve(func(args));
+      }, delay);
+    });
 
-	const teardown = () => clearTimeout(timer);
+  const teardown = () => clearTimeout(timer);
 
-	return [debouncedFunc, teardown];
+  return [debouncedFunc, teardown];
 }
