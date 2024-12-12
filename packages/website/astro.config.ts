e043c2939ccbfb63a78241269fc9c50d9f13dc3f
Billy Le <hi@billyle.dev>
2024-12-12 12:13:24 +0800

    add sentry

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -6,28 +6,38 @@ import remarkSectionize from "remark-sectionize";
 import { remarkReadingTime } from "./remark-plugins/remark-reading-time";
 import { remarkLastDateModified } from "./remark-plugins/remark-last-date-modified";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 /** @type {import('astro/config').AstroUserConfig} */
 export default defineConfig({
-	site: "https://billyle.dev",
-	integrations: [
-		tailwind({
-			nesting: true,
-		}),
-		sitemap(),
-	],
-	prefetch: true,
-	markdown: {
-		shikiConfig: {
-			wrap: true,
-		},
-		remarkPlugins: [
-			remarkSectionize,
-			remarkReadingTime,
-			remarkLastDateModified,
-		],
-	},
-	redirects: {
-		"/posts": "/blog",
-	},
+  site: "https://billyle.dev",
+  integrations: [
+    tailwind({
+      nesting: true,
+    }),
+    sitemap(),
+    sentry({
+      dsn: "https://00989e74aa26bc5066973c592ab89a34@o4507544338694144.ingest.us.sentry.io/4508280812601344",
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
+  prefetch: true,
+  markdown: {
+    shikiConfig: {
+      wrap: true,
+    },
+    remarkPlugins: [
+      remarkSectionize,
+      remarkReadingTime,
+      remarkLastDateModified,
+    ],
+  },
+  redirects: {
+    "/posts": "/blog",
+  },
 });
+
