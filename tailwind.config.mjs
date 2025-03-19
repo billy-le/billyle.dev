d47b2a6b3c9afdeb723f0d7bfdb81fc1a273b61e
Billy Le <hi@billyle.dev>
2025-03-19 18:49:52 +0800

    finished migration to tailwindcss v4

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,5 @@
-import typography from "@tailwindcss/typography";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [typography()],
 };
